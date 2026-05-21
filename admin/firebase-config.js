724ebee8472504baed59627a67c33f7b9b126d18
Shreyash Mane <anosvoldigoadw1@gmail.com>
2026-05-21 20:26:24 +0000

    Commit admin Firebase config so admin login works

---
+++ admin/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAo5Vaq22LVBMVMBlOLLdzOWvgbXHiu3Sk",
+  authDomain: "shield-focus.firebaseapp.com",
+  projectId: "shield-focus",
+  storageBucket: "shield-focus.firebasestorage.app",
+  messagingSenderId: "404953360336",
+  appId: "1:404953360336:web:1fd1998192292cb4577bd5",
+  measurementId: "G-LQG7CCYDX3"
+};
